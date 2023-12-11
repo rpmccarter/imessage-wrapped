10b5ba23d51b6bcc88ac04b4c305609f29b54bae
David Lane <davidlanedc@gamil.com>
2023-12-10 20:58:08 -0800

    Stepped approach to uploads

--- website/src/app/upload/page.tsx
+++ website/src/app/upload/page.tsx
@@ -9,10 +9,18 @@ import { useRouter } from 'next/navigation';
 import { BeatLoader } from 'react-spinners';
 import { QueryManager } from '@/db/QueryManager';
 
-const steps = [
-  'download your phone data to your laptop',
-  'export a file containing your chats',
-  'export a file with your contact data',
+const dbSteps = [
+  'Copy the following text: ~/Library/Messages/',
+  'Hit “command + space” to open the Spotlight search.',
+  'Paste the text into the search bar, then open the "Messages" folder (search result)',
+  'Locate the chat.db file, and drag it into the upload box to the right',
+];
+
+const contactSteps = [
+  'Open the Contacts app on your Mac',
+  '"Command + A" to select all contacts',
+  'Right click, then click "Export vCard..."',
+  'Choose a destination to save the file, then upload or drag it into the box to the right',
 ];
 
 export default function GettingStarted() {
@@ -21,31 +29,117 @@ export default function GettingStarted() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-24">
-      <h1 className="text-4xl">how to get your wrapped</h1>
+      <h1 className="text-4xl">Let's get your iMessages wrapped</h1>
+
       <div className="flex gap-6">
-        <div className="flex flex-col m-4 gap-6">
-          {steps.map((value, index) => (
-            <Step num={index + 1} key={index}>
-              {value}
-            </Step>
-          ))}
+        <div className="flex flex-col m-4 gap-6 w-2/3">
+          <div className="flex flex-row gap-6">
+            <h2 className="text-2xl">1. Upload your iMessages</h2>
+            {chatFile ? (
+              <div>
+                <h2 className="text-2xl text-green-500">&#10003;</h2>
+              </div>
+            ) : null}
+          </div>
+          {chatFile ? (
+            <div className="flex flex-row gap-6">
+              <h2 className="text-2xl">2. Add your contacts</h2>
+              {contactFile ? (
+                <div>
+                  <h2 className="text-2xl text-green-500">&#10003;</h2>
+                </div>
+              ) : null}
+            </div>
+          ) : null}
+          {!chatFile && !contactFile ? (
+            <div className="flex flex-col gap-6">
+              {dbSteps.map((value, index) => (
+                <Step num={index + 1} key={index}>
+                  {value}
+                </Step>
+              ))}
+            </div>
+          ) : null}
+          {chatFile && !contactFile ? (
+            <div className="flex flex-col gap-6">
+              {contactSteps.map((value, index) => (
+                <Step num={index + 1} key={index}>
+                  {value}
+                </Step>
+              ))}
+            </div>
+          ) : null}
         </div>
-        <div className="flex flex-col">
-          <FileInput
-            title="chat.db file"
-            accept=".db,.gz"
-            value={chatFile}
-            setValue={setChatFile}
-          />
-          <FileInput
-            title="contacts file"
-            accept=".vcf"
-            value={contactFile}
-            setValue={setContactFile}
-          />
+
+        <div className="flex flex-col w-1/3">
+          {!chatFile || !contactFile ? (
+            <div
+              style={{
+                aspectRatio: '1 / 1',
+                width: '100%',
+              }}
+              className={clsx(
+                'text-lg font-mono text-ellipsis overflow-hidden',
+                'text-gray-400 dark:text-white/30',
+                'flex items-center justify-center',
+              )}
+            >
+              (gif goes here)
+            </div>
+          ) : null}
+
+          {!chatFile ? (
+            <FileInput
+              title="chat.db file"
+              accept=".db,.gz"
+              value={chatFile}
+              setValue={setChatFile}
+            />
+          ) : (
+            <div style={{ height: '75px' }}>
+              <div
+                className={clsx(
+                  'text-lg font-mono text-ellipsis overflow-hidden',
+                  'text-gray-400 dark:text-white/30',
+                  'flex items-center justify-center h-full',
+                )}
+              >
+                {chatFile.name ?? `Drop your file here or click to upload`}
+                &#10003;
+              </div>
+            </div>
+          )}
+          {chatFile && !contactFile ? (
+            <FileInput
+              title="contacts file"
+              accept=".vcf"
+              value={contactFile}
+              setValue={setContactFile}
+            />
+          ) : null}
+          {chatFile && contactFile ? (
+            <div style={{ height: '100px' }}>
+              <div
+                className={clsx(
+                  'text-lg font-mono text-ellipsis overflow-hidden',
+                  'text-gray-400 dark:text-white/30',
+                )}
+              >
+                {contactFile.name ?? `Drop your file here or click to upload`}
+                &#10003;
+              </div>
+            </div>
+          ) : null}
         </div>
       </div>
-      <SubmitButton chatFile={chatFile} contactFile={contactFile} />
+      {chatFile && contactFile ? (
+        <SubmitButton chatFile={chatFile} contactFile={contactFile} />
+      ) : null}
+
+      <h3>
+        Don’t worry, we NEVER store or save your data. All of the magic is done
+        on your own device, so we never even see it.
+      </h3>
     </main>
   );
 }
@@ -67,7 +161,7 @@ const SubmitButton = ({ chatFile, contactFile }: SubmitButtonProps) => {
           : undefined
       }
       className={clsx(
-        'border px-4 py-2 rounded-full items-center ',
+        'border px-4 py-2 rounded-full items-center w-1/3',
         chatFile === undefined
           ? 'text-gray-600 bg-gray-600/20 border-gray-600 pointer-events-none'
           : 'text-sky-600 bg-sky-600/20 border-sky-600',
@@ -110,7 +204,7 @@ type StepProps = {
 const Step = ({ num, children }: StepProps) => {
   return (
     <div className="relative">
-      <div className="p-6 text-3xl bg-sky-600 rounded-3xl">{children}</div>
+      <div className="p-6 text-2xl bg-sky-600 rounded-3xl">{children}</div>
       <Tail />
     </div>
   );
