cf89e1fd62b5d7bd5d59be98afd8056707fc04d7
David Lane <davidlanedc@gamil.com>
2023-12-13 23:15:07 -0800

    starting changes

--- website/src/app/results/Slide.tsx
+++ website/src/app/results/Slide.tsx
@@ -8,6 +8,7 @@ import {
 import { BarElement, CategoryScale, Chart, LinearScale } from 'chart.js';
 import { ReactNode, useState } from 'react';
 import { Bar } from 'react-chartjs-2';
+import bg1 from './ExampleForMatt.png';
 
 Chart.register(CategoryScale);
 Chart.register(LinearScale);
@@ -44,14 +45,26 @@ export const SummarySlide = ({ index, data }: SummarySlideProps) => {
       id={`result-${index}`}
       className="p-10 flex flex-col w-full h-full bg-orange-400 items-center justify-center gap-4"
     >
-      <h1 className="text-5xl">This year you sent</h1>
+      <h1 className="text-3xl">This year you sent</h1>
       <h1 className="text-6xl font-bold">
         {numberWithCommas(data.total_texts_sent)}
       </h1>
-      <h2 className="text-5xl">messages</h2>
-      <div className="pt-8 text-3xl text-center text-balance">{copy}</div>
+      <h2 className="text-3xl">messages</h2>
+      <div className="pt-8 text-2xl text-center text-balance">{copy}</div>
     </div>
   );
+};
+
+const getCopyForCount = (n: number) => {
+  if (n < 25_000) {
+    return 'You really need more friends. Can’t help you with that, but there may be some paid sites!';
+  }
+
+  if (n < 50_000) {
+    return 'According to our calculations, you are very average. Take that as you will.';
+  }
+
+  return 'Congrats! You’re an iMessage socialite. An addict, some might say. Feel free to text your friends about it, because you’re probably compelled to. You probably already did.';
 };
 
 type ByMonthSlideProps = {
@@ -79,20 +92,39 @@ export const ByMonthSlide = ({ index, data }: ByMonthSlideProps) => {
     ([a], [b]) => months.indexOf(a) - months.indexOf(b),
   );
   const labels = sortedMonths.map(([key]) => key);
-  const datasets = [{ data: sortedMonths.map(([_, val]) => val) }];
+  const datasets = [
+    { data: sortedMonths.map(([_, val]) => val), backgroundColor: 'white' },
+  ];
+
+  const options = {
+    scales: {
+      x: {
+        ticks: {
+          //font: { weight: 'bold' },
+          color: 'white', // Change X-axis labels to red
+        },
+      },
+      y: {
+        ticks: {
+          maxTicksLimit: 5,
+          color: 'white', // Change Y-axis labels to blue
+        },
+      },
+    },
+  };
 
   const { topMonth, count } = findMaxMonth(data);
   return (
     <div
       key={index}
       id={`result-${index}`}
-      className="p-10 flex flex-col w-full h-full bg-orange-400 items-center justify-center gap-4"
+      className="p-5 flex flex-col w-full h-full bg-orange-400 items-center justify-center gap-4"
     >
-      <h1 className="leading-tight text-5xl text-center">
+      <h1 className="leading-tight text-3xl text-center">
         Your busiest month was {topMonth}, where you sent{' '}
         {numberWithCommas(count)} messages
       </h1>
-      <Bar data={{ labels, datasets }} />
+      <Bar data={{ labels, datasets }} options={options} />
     </div>
   );
 };
@@ -119,18 +151,39 @@ type TopSendersSlideProps = {
 
 export const TopSendersSlide = ({ index, data }: TopSendersSlideProps) => {
   const labels = data.map(({ id }) => id);
-  const datasets = [{ data: data.map(({ messages_sent }) => messages_sent) }];
+  const datasets = [
+    {
+      data: data.map(({ messages_sent }) => messages_sent),
+      backgroundColor: 'white',
+    },
+  ];
 
+  const options = {
+    scales: {
+      x: {
+        ticks: {
+          //font: { weight: 'bold' },
+          color: 'white', // Change X-axis labels to red
+        },
+      },
+      y: {
+        ticks: {
+          maxTicksLimit: 5,
+          color: 'white', // Change Y-axis labels to blue
+        },
+      },
+    },
+  };
   return (
     <div
       key={index}
       id={`result-${index}`}
-      className="p-10 flex flex-col w-full h-full bg-orange-400 items-center justify-center gap-4"
+      className="p-5 flex flex-col w-full h-full bg-orange-400 items-center justify-center gap-4"
     >
-      <h1 className="leading-tight text-5xl text-center">
+      <h1 className="leading-tight text-3xl text-center">
         {"Here's who you've texted this year. You should text your mom more."}
       </h1>
-      <Bar data={{ labels, datasets }} />
+      <Bar data={{ labels, datasets }} options={options} />
     </div>
   );
 };
@@ -149,28 +202,31 @@ export const TopFriendsSlide = ({ index, data }: TopFriendsSlideProps) => {
     <div
       key={index}
       id={`result-${index}`}
-      className="p-10 flex flex-col w-full h-full bg-sky-400 items-center justify-center gap-4"
+      className="relative  flex flex-col w-full h-full bg-sky-400 items-center justify-center gap-4"
     >
-      <h1 className="leading-tight text-5xl text-center">
-        We all have favorites. Your three best friends are:
-      </h1>
-      <div className="flex flex-col gap-2">
-        {friends.map(([name, { message_count }], i) => {
-          return (
-            <div key={i} className="flex gap-6">
-              <div className="flex gap-2">
-                <div className="text-6xl">{i + 1}</div>
-                <div className="text-4xl">{name}</div>
-              </div>
-              <div>
-                <div className="text-4xl">
-                  {numberWithCommas(message_count)}
+      <img className="" src={bg1.src}></img>
+      <div className="absolute p-10 flex flex-col gap-4">
+        <h1 className="leading-tight text-3xl text-center ">
+          We all have favorites. Your three best friends are...
+        </h1>
+        <div className="flex flex-col gap-2 w-full">
+          {friends.map(([name, { message_count }], i) => {
+            return (
+              <div key={i} className="flex gap-6 ">
+                <div className="flex gap-2">
+                  <div className="text-4xl">{i + 1}</div>
+                  <div className="text-3xl">{name}</div>
                 </div>
-                <div>messages</div>
+                {/*<div className="bg-sky-400 p-2 rounded-lg items-center flex flex-col">
+                  <div className="text-2xl">
+                    {numberWithCommas(message_count)}
+                  </div>
+                  <div className="text-1xl">messages</div>
+            </div>*/}
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
@@ -215,51 +271,43 @@ export const FriendSummarySlide = ({
       id={`result-${index}`}
       className="p-10 flex flex-col w-full h-full bg-sky-400 items-center justify-center gap-4"
     >
-      <h1 className="leading-tight text-5xl text-center">
+      <h1 className="leading-tight text-3xl text-center">
         {"Let's talk about " + id + '.'}
       </h1>
-      <h1 className="leading-tight text-2xl text-center">
-        Your top words were:
-      </h1>
-      <div className="flex flex-col gap-1">
-        {Object.entries(data.top_word_count).map(([word], i) => {
-          return (
-            <div key={i} className="flex gap-2">
-              <div className="text-2xl leading-none">{i + 1}.</div>
-              <div className="text-2xl leading-none">{word}</div>
-            </div>
-          );
-        })}
+      <div className="flex flex-row gap-10">
+        <div className="flex flex-col gap-2 items-center">
+          <h1 className="leading-tight text-2xl text-center">Top words:</h1>
+          <div className="flex flex-col gap-4">
+            {Object.entries(data.top_word_count).map(([word], i) => {
+              return (
+                <div key={i} className="flex gap-3">
+                  <div className="text-2xl leading-none">{i + 1}.</div>
+                  <div className="text-2xl leading-none">{word}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        {/*<h1 className="leading-tight text-1xl text-center">{quip1}</h1>*/}
+        <div className="flex flex-col gap-2 ">
+          <h1 className="leading-tight text-2xl text-center">Top emojis:</h1>
+          <div className="flex flex-col gap-2 items-center">
+            {Object.entries(data.top_emojis).map(([emoji], i) => {
+              return (
+                <div key={i} className="flex gap-4">
+                  <div className="text-2xl">{i + 1}.</div>
+                  <div className="text-2xl">{emoji}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
+
       <h1 className="leading-tight text-1xl text-center">{quip1}</h1>
-      <h1 className="leading-tight text-2xl text-center">
-        Your top emojis were:
-      </h1>
-      <div className="flex flex-col gap-2">
-        {Object.entries(data.top_emojis).map(([emoji], i) => {
-          return (
-            <div key={i} className="flex gap-2">
-              <div className="text-2xl">{i + 1}.</div>
-              <div className="text-2xl">{emoji}</div>
-            </div>
-          );
-        })}
-      </div>
-      <h1 className="leading-tight text-1xl text-center">{quip2}</h1>
     </div>
   );
-};
-
-const getCopyForCount = (n: number) => {
-  if (n < 25_000) {
-    return 'you really need more friends. Can’t help you with that, but there may be some paid sites!';
-  }
-
-  if (n < 50_000) {
-    return 'according to our calculations, you are very average. Take that as you will';
-  }
-
-  return 'congrats! You’re an iMessage socialite. An addict, some might say. Feel free to text your friends about it, because you’re probably compelled to. You probably already did.';
 };
 
 function numberWithCommas(x: number) {
